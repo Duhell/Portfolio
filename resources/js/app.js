148706f8dfc2ea297bda76957d33973af527ecef
Duhell <ryanvillarma148@gmail.com>
2024-06-06 21:53:02 +0800

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,11 @@ import "./about.js";
 import "./projects.js";
 import "./contact.js";
 
+let currentTitle = "Ryan Villarma";
 window.addEventListener('DOMContentLoaded',()=> AOS.init());
 window.addEventListener('changeTitle', event =>{
    event.detail === "Home" ? document.title = "Ryan Villarma" : document.title  = `${event.detail} | Ryan Villarma`;
+   currentTitle = document.title;
 })
+window.addEventListener('blur',()=>document.title = "Am I good enough?");
+window.addEventListener('focus',()=>document.title = currentTitle );
